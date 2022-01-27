003577e5d84adb30d8499178bd6a2cbbe82aa18d
NishatFariza <nishatfarzia78@gmail.com>
2022-01-27 16:48:02 +0600

    array index number and element practice

---
+++ array-index.js
@@ -0,0 +1,16 @@
+// index number 
+var bookpages =['hablo', 'tablo', 'bolod', 'sodol', 'paracitamol'];
+var bolidIndexNumber= bookpages.indexOf('bolod');
+console.log(bolidIndexNumber);
+
+// element number 
+var bookpages =['hablo', 'tablo', 'bolod', 'sodol', 'paracitamol'];
+var elementNumber= bookpages[3];
+console.log(elementNumber);
+
+// new element add 
+var numbers =[22, 35, 45, 61, 72, 48, 21, 98, 32];
+numbers[1]=95;
+numbers[7]=20;
+console.log(numbers);
+
