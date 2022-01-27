f839ef096c2a2a55899621f60ab005dabd665c43
NishatFariza <nishatfarzia78@gmail.com>
2022-01-27 15:19:59 +0600

    variable and arrey practice

---
+++ array.js
@@ -0,0 +1,15 @@
+// Array number 
+var friendAge =[11, 14, 15, 17];
+var muriChanachurPrice=[55, 78.150];
+var oddNumber =[1, 3, 5, 7];
+
+// Array string 
+var nayikas=['mahi', 'mim', 'joya',];
+var vowels=['a', 'e', 'i', 'o', 'u'];
+
+// output
+console.log(friendAge);
+console.log(muriChanachurPrice);
+console.log(oddNumber);
+console.log(nayikas);
+console.log(vowels);
---
+++ variable.js
@@ -0,0 +1,9 @@
+// number 
+var watchPrice =150;
+watchPrice =250;
+
+// String 
+var fistName='Nishat Fariza';
+
+// Boolean 
+var isExpensive =True;
