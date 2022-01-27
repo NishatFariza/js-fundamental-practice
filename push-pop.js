2911683013a854923b62f7585728deb17ca95ed0
NishatFariza <nishatfarzia78@gmail.com>
2022-01-27 19:34:16 +0600

    push, pop, shift and unshift practice done

---
+++ push-pop.js
@@ -0,0 +1,19 @@
+// "push" add element an array in the last 
+var lastBranch=['jamal', 'kamal', 'samal', ];
+lastBranch.push('sajal');
+console.log(lastBranch);
+ 
+// "pop" remove element an Array in the last 
+var friendsAge=[12, 24, 35, 23, 21, 32];
+friendsAge.pop();
+console.log(friendsAge);
+
+// "shift" remove an element in array in the first 
+var catsAge=[1, 4, 3, 5, 7, 10];
+catsAge.shift();
+console.log(catsAge);
+
+// "unshift" add an element in array in the first 
+var catsName =['Tom', 'Boby', 'puff', 'willy'];
+catsName.unshift('George');
+console.log(catsName);
