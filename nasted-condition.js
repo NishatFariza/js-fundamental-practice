9d64ab4bbe4e896bd82f1c87ff32f3eab0c669a6
NishatFariza <nishatfarzia78@gmail.com>
2022-01-28 17:18:51 +0600

    nasted conditions concept clear

---
+++ nasted-condition.js
@@ -0,0 +1,34 @@
+//  nasted condition if and else
+//  var danishPrice =50;
+//  var butterBreadPrice=25;
+//  var toastBiscutPrice=10;
+//  var myBudget=100;
+
+//  if (danishPrice < myBudget){
+//      console.log('danish kheye danish jabo');
+//  }
+// else{
+//     console.log('Shodhu tea khabo');
+// }
+
+
+//  nasted condition if and else if and else
+ var danishPrice =50;
+ var butterBreadPrice=25;
+ var toastBiscutPrice=10;
+ var myBudget=100;
+
+ if (danishPrice > myBudget){
+     console.log('danish kheye danish jabo');
+ }
+ else if(butterBreadPrice > myBudget){
+     console.log('Butter bread diye tea khabo');
+
+ }
+ else if(toastBiscutPrice < myBudget){
+     console.log('Toast diye tea khabo');
+
+ }
+else{
+    console.log('Shodhu tea khabo');
+}
