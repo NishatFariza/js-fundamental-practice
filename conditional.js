80ad5d9532a262538c638a3b384740d634036ed0
NishatFariza <nishatfarzia78@gmail.com>
2022-01-28 14:33:09 +0600

    multiple conditions practice done

---
+++ conditional.js
@@ -0,0 +1,35 @@
+var iphonePrice =70000;
+var myBadget =9500;
+if (iphonePrice > myBadget){
+    console.log('Iphone diye pic tule fotani marbo');
+
+}
+var iphonePrice =70000;
+var myBadget =109500;
+if (iphonePrice < myBadget){
+    console.log('Iphone amar na hoyleo hoto tobo nilam arki ');
+
+}
+// var chickenPrice =140;
+// var myMoney=250;
+// if (chickenPrice < myMoney){
+//     console.log('chicken paile khabo');
+// }
+// if (chickenPrice > myMoney){
+//     console.log('chicken paile khabo');
+// }
+// else{
+//     console.log('smashed potato with lentil soup');
+// }
+var age =23;
+var vaccineCount=2;
+var isVaccinate =false;
+if (isVaccinate){
+    console.log('chicken paile khabo');
+}
+if (chickenPrice > myMoney){
+    console.log('chicken paile khabo');
+}
+else{
+    console.log('smashed potato with lentil soup');
+}
---
+++ multiple-condition.js
@@ -0,0 +1,81 @@
+// condition jodi true hoi 
+// var  gotJob =true;
+// if (gotJob == true){
+//     console.log('Cholo biye kore feli');
+// }
+
+// condition jodi false hoi 
+// var  gotJob =true;
+// if (gotJob == false){
+//     console.log('Cholo biye kore feli');
+// }
+// else{
+//     console.log('Tor kopale biye nai');
+// }
+
+// doita condition ey jodi true hoi 
+// var  gotJob =true;
+// var moneySave = 250000;
+// if (gotJob == true && moneySave >200000){
+//     console.log('Cholo biye kore feli');
+// }
+// else{
+//     console.log('Tor kopale biye nai');
+// }
+
+// doita condition er ekta jodi false hoi 
+// var  gotJob =true;
+// var moneySave = 250000;
+// if (gotJob == true && moneySave >300000){
+//     console.log('Cholo biye kore feli');
+
+// }
+// else{
+//     console.log('Tor kopale biye nai');
+// }
+
+//jodi condition 3 ta thake 3tai jodi true hoi 
+// var  gotJob =true;
+// var moneySave = 250000;
+// var hasFlat =true;
+// if (gotJob == true && moneySave >200000 && hasFlat==true){
+//     console.log('Cholo biye kore feli');
+
+// }
+// else{
+//     console.log('Tor kopale biye nai');
+// }
+//jodi condition 3 ta thake 3tar ekta jodi false hoi 
+// var  gotJob =true;
+// var moneySave = 250000;
+// var hasFlat =true;
+// if (gotJob == true && moneySave >200000 && hasFlat==false){
+//     console.log('Cholo biye kore feli');
+
+// }
+// else{
+//     console.log('Tor kopale biye nai');
+// }
+
+//jodi condition 2 ta thake 2tar ekta jodi true hoi 
+var  gotJob =true;
+var moneySave = 250000;
+if (gotJob == true || moneySave >300000){
+    console.log('Cholo biye kore feli');
+
+}
+else{
+    console.log('Tor kopale biye nai');
+}
+
+// jodi condition 3 ta thake 3tar ekta jodi true hoi 
+var  gotJob =true;
+var moneySave = 250000;
+var hasFlat =true;
+if ((gotJob == true && moneySave >200000) || hasFlat==false){
+    console.log('Cholo biye kore feli');
+
+}
+else{
+    console.log('Tor kopale biye nai');
+}
